47ef11163bc24c14e954a1546260064e28957dc6
hakbar0 <haseeb2000@hotmail.com>
2023-03-04 20:51:15 +0000

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/controllers/auth.controller';
-import { AuthService } from './auth/services/auth.service';
 import { AuthModule } from './auth/auth.module';
-import { User } from './auth/entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import entities from './auth/entities/entities';
-import { JwtModule } from '@nestjs/jwt';
-
 @Module({
-  imports: [
-    AuthModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASS,
-      database: process.env.DB_DATABASE,
-      entities,
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRY },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [AuthModule],
 })
 export class AppModule {}
